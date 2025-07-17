4e981e1b06d78bcce72cfa91a2564bdc5a6bd250
엄경문 <eomkyeongmun@naver.com>
2025-07-17 20:25:06 +0900

    refactor:openApiConfig 수정

--- src/main/java/com/example/firstproject/config/OpenApiConfig.java
+++ src/main/java/com/example/firstproject/config/OpenApiConfig.java
@@ -1,8 +1,6 @@
 package com.example.firstproject.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
